d1258d893a9055a9a089aaa81c69a657072efec5
Stephan-404 <stephanps14@gmail.com>
2020-10-09 08:44:57 +0200

    Initial commit

---
+++ src/htbla/aud3/graphtheory/Edge.java
@@ -0,0 +1,16 @@
+package htbla.aud3.graphtheory;
+
+/*
+ * @author Andreas, David, Stephan
+ */
+public class Edge {
+    
+    public int getFromNodeId() {
+        return -1;
+    }
+    
+    public int getToNodeId() {
+        return -1;
+    }
+    
+}
---
+++ src/htbla/aud3/graphtheory/Path.java
@@ -0,0 +1,16 @@
+package htbla.aud3.graphtheory;
+
+/**
+ * @author Andreas, David, Stephan
+ */
+public class Path {
+    
+    public int[] getNodeIds() {
+        return null;
+    }
+    
+    public double computeDistance() {
+        return -1.0;
+    }
+    
+}
